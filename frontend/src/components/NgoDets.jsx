d9fc03b0243b5b875119f768d2890c56f3fd9077
SID <bahetisid06@gmail.com>
2023-12-02 11:52:48 +0530

    form

--- frontend/src/components/NgoDets.jsx
+++ frontend/src/components/NgoDets.jsx
@@ -30,12 +30,12 @@ const NgoDets = () => {
         <div className={styles.leftinfo}>
           <p className={styles.name}>Name XXX XXX</p>
           <p className={styles.loct}>Location</p>
-          <p>
+          <p style={{ fontSize: "1.1vw" }}>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam rem nemo maiores perferendis distinctio
             repellendus corrupti aperiam cupiditate voluptatibus vitae eos ea, totam dignissimos tempore quam sint est,
             eveniet impedit.
           </p>
-          <p style={{textAlign : "center", paddingTop : "1.5vh"}}><a href='https://meet.google.com/?authuser=0' className={styles.link}>For more info</a></p>
+          <p style={{ textAlign: "center", paddingTop: "2vh" }}><a href='https://meet.google.com/?authuser=0' className={styles.link}>For more info</a></p>
         </div>
       </div>
 
@@ -44,27 +44,42 @@ const NgoDets = () => {
         <p className={styles.heading}>Kindly fill in the below details</p>
 
         <div className={styles.rightbg}>
-        
-          {!location.latitude ?
-            <div className={styles.location}>
-            <p>Location : </p>
-            <button onClick={getLocation} className={styles.button}>Get Location</button>
-            </div>
-            :
-            (<>
-              <p>Coordinates: {location.latitude} , {location.longitude} </p>
-              </>
-            )
-          }
 
-          <input placeholder='Name' type='text' className={styles.input} />
-          <input placeholder='Age' type='number' className={styles.input} />
-          <input placeholder='Amount' type='number' className={styles.input} />
-          <input placeholder='Email' type='email' className={styles.input} />
-          <input placeholder='Number' type='number' className={styles.input} />
-          <input placeholder='Occupation' type='number' className={styles.input} />
-          <input placeholder='Gender' type='number' className={styles.input} />
+          <div className={styles.rightinput}>
+            <input placeholder='Name' type='text' className={styles.input} />
+            <input placeholder='Age' type='number' className={styles.input} />
+            <input placeholder='Email' type='email' className={styles.input} />
+            <input placeholder='Number' type='number' className={styles.input} />
+            <select defaultValue="" >
+              <option disabled hidden value="">Select Gender</option>
+              <option value="male">Male</option>
+              <option value="female">Female</option>
+              <option value="other">Other</option>
+            </select>
+            <br/>
+            <select defaultValue="" >
+              <option disabled hidden value="">Select Occupation</option>
+              <option value="male">Employed</option>
+              <option value="female">Unemployed</option>
+              <option value="other">Self-Employed</option>
+              <option value="other">Student</option>
+              <option value="other">Retired</option>
+            </select>
+            <input placeholder='Amount' type='number' className={styles.input} />
+          </div>
+
+          <div className={styles.location}>
+            <p>Location : </p>
+            {!location.latitude ?
+              <p onClick={getLocation} className={styles.cord1}>Get Location</p>
+              :
+              <p className={styles.cord2}>{location.latitude} , {location.longitude}</p>
+            }
+          </div>
+
+          <button onClick={getLocation} className={styles.button}>Submit</button>
         </div>
+
       </div>
     </div>
   );
