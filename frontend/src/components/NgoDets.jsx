69f0c22c97606bdaf435c5107e979a025f2e7569
Dhruv Sapra <dhruvsapra88@gmail.com>
2023-12-02 12:15:18 +0530

    added dropdown

--- frontend/src/components/NgoDets.jsx
+++ frontend/src/components/NgoDets.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import styles from "../stylesheets/NgoDets.module.css";
 import img1 from "../assets/temp1.jpg";
 
@@ -11,7 +11,7 @@ const NgoDets = () => {
         (position) => {
           setlocation({
             latitude: position.coords.latitude,
-            longitude: position.coords.longitude
+            longitude: position.coords.longitude,
           });
         },
         (error) => {
@@ -31,55 +31,82 @@ const NgoDets = () => {
           <p className={styles.name}>Name XXX XXX</p>
           <p className={styles.loct}>Location</p>
           <p style={{ fontSize: "1.1vw" }}>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam rem nemo maiores perferendis distinctio
-            repellendus corrupti aperiam cupiditate voluptatibus vitae eos ea, totam dignissimos tempore quam sint est,
-            eveniet impedit.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+            rem nemo maiores perferendis distinctio repellendus corrupti aperiam
+            cupiditate voluptatibus vitae eos ea, totam dignissimos tempore quam
+            sint est, eveniet impedit.
           </p>
-          <p style={{ textAlign: "center", paddingTop: "2vh" }}><a href='https://meet.google.com/?authuser=0' className={styles.link}>For more info</a></p>
+          <p style={{ textAlign: "center", paddingTop: "2vh" }}>
+            <a
+              href="https://meet.google.com/?authuser=0"
+              className={styles.link}
+            >
+              For more info
+            </a>
+          </p>
         </div>
       </div>
 
       <div className={styles.right}>
-
         <p className={styles.heading}>Kindly fill in the below details</p>
 
         <div className={styles.rightbg}>
-
           <div className={styles.rightinput}>
-            <input placeholder='Name' type='text' className={styles.input} />
-            <input placeholder='Age' type='number' className={styles.input} />
-            <input placeholder='Email' type='email' className={styles.input} />
-            <input placeholder='Number' type='number' className={styles.input} />
-            <select defaultValue="" >
-              <option disabled hidden value="">Select Gender</option>
+            <input placeholder="Name" type="text" className={styles.input} />
+            <input placeholder="Age" type="number" className={styles.input} />
+            <input placeholder="Email" type="email" className={styles.input} />
+            <input
+              placeholder="Number"
+              type="number"
+              className={styles.input}
+            />
+            <br />
+            <select defaultValue="" className={styles.customdropdown}>
+              <option disabled hidden value="">
+                Select Gender
+              </option>
               <option value="male">Male</option>
               <option value="female">Female</option>
               <option value="other">Other</option>
             </select>
-            <br/>
-            <select defaultValue="" >
-              <option disabled hidden value="">Select Occupation</option>
+            <br />
+            <br />
+            <select defaultValue="" className={styles.customdropdown}>
+              <option disabled hidden value="">
+                Select Occupation
+              </option>
               <option value="male">Employed</option>
               <option value="female">Unemployed</option>
               <option value="other">Self-Employed</option>
               <option value="other">Student</option>
               <option value="other">Retired</option>
             </select>
-            <input placeholder='Amount' type='number' className={styles.input} />
+            <br />
+            <br />
+            <input
+              placeholder="Amount"
+              type="number"
+              className={styles.input}
+            />
           </div>
 
           <div className={styles.location}>
             <p>Location : </p>
-            {!location.latitude ?
-              <p onClick={getLocation} className={styles.cord1}>Get Location</p>
-              :
-              <p className={styles.cord2}>{location.latitude} , {location.longitude}</p>
-            }
+            {!location.latitude ? (
+              <p onClick={getLocation} className={styles.cord1}>
+                Get Location
+              </p>
+            ) : (
+              <p className={styles.cord2}>
+                {location.latitude} , {location.longitude}
+              </p>
+            )}
           </div>
 
-          <button onClick={getLocation} className={styles.button}>Submit</button>
+          <button onClick={getLocation} className={styles.button}>
+            Submit
+          </button>
         </div>
-
       </div>
     </div>
   );
